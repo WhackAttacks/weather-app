da15b1bc36eeecda62a47af9acf6cfdd7495be3b
Kobi Satamker <kkobista0@gmail.com>
2024-03-14 18:58:43 +0200

    css/js-start api

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
-fetch('http://api.openweathermap.org/data/2.5/weather?id=524901&appid=51ba1058f88521716afaf7b9c9565a49')
-  .then(res => res.json())
-  .then(data => console.log(data))
-
-fetch('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=51ba1058f88521716afaf7b9c9565a49')
-  .then(res => res.json())
-  .then(data => console.log(data))
+  let weather = {
+    "apiKey": "445ff49c1a8026ac212846be574b710d",
+    fetchWeather: function(city) {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}`
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data))
+    },
+    displayWeather: function(data) {
+      const { name} = data;
+      const {icon, description} = data.weather[0];
+      const {temp, humidity} = data.main;
+      const {speed} = data.wind;
+      console.log(name,icon,description,temp,humidity,speed)
+      // document.querySelector(".city").innerText = `Weather in ${name}`;
+      // document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}.png`
+      // document.querySelector(".description").innerText = description;
+      // document.querySelector(".temp").innerText = `${temp}`
+      // document.querySelector(".humidity").innerText = `Humidity ${humidity}%`
+      // document.querySelector('.wind').innerText = `Wind speed ${speed} km/h`
+    }
+  }
